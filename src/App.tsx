5b4445b3e705f9babc1696c8b8a211c5565c577d
5440000 <5440000@gmail.com>
2025-07-13 15:39:28 +0200

    feat: set up base class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-class App extends React.Component<{}, {}> {
+class App extends React.Component<
+  Record<string, never>,
+  Record<string, never>
+> {
   render() {
     return (
       <>
